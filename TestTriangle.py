311d600d8951292c87fcf60588d74242cf7b3479
Khusboo Patel <kkp5601@gmail.com>
2023-02-10 09:50:52 -0500

    Commit after changing the code

--- TestTriangle.py
+++ TestTriangle.py
@@ -23,8 +23,35 @@ class TestTriangles(unittest.TestCase):
     def testRightTriangleB(self): 
         self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
         
-    def testEquilateralTriangles(self): 
+    def testEquilateralTrianglesA(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+    
+    def testEquilateralTrianglesB(self): 
+        self.assertEqual(classifyTriangle(3,3,3),'Equilateral','3,3,3 should be equilateral')
+        
+    def testIsocelesTriangleA(self): 
+        self.assertEqual(classifyTriangle(2,1,1),'Isoceles','2,1,1 should be isoceles')    
+
+    def testIsocelesTriangleB(self): 
+        self.assertEqual(classifyTriangle(1,2,1),'Isoceles','1,2,1 should be isoceles')
+
+    def testScaleneTrianglesA(self): 
+        self.assertEqual(classifyTriangle(2,3,1),'Scalene','2,3,1 should be scalene') 
+    
+    def testScaleneTrianglesB(self): 
+        self.assertEqual(classifyTriangle(2,5,6),'Scalene','2,5,6 should be scalene') 
+
+    def testInvalidInputA(self):
+        self.assertEqual(classifyTriangle(202,1,1),'InvalidInput','202,1,1 should be invalid')
+
+    def testInvalidInputB(self):
+        self.assertEqual(classifyTriangle(-1,2,1),'InvalidInput','-1,2,1 should be invalid')
+
+    def testInvalidInputC(self):
+        self.assertEqual(classifyTriangle(1.5,3,2),'InvalidInput','1.5,3,2 should be invalid')
+
+    def testInvalidInputD(self):
+        self.assertEqual(classifyTriangle(1,2,15),'NotATriangle','1,2,15 should not be a triangle')  
 
 if __name__ == '__main__':
     print('Running unit tests')
